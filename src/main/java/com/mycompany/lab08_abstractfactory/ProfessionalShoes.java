c302c3e11574a9bb6cbb48456d14471bf32748fe
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-21 17:52:49 -0400

    Added professional shoes class that implements Shoes.

---
+++ src/main/java/com/mycompany/lab08_abstractfactory/ProfessionalShoes.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab08_abstractfactory;
+
+/**
+ *
+ * @author canen
+ */
+public class ProfessionalShoes implements Shoes
+{
+    @Override
+    public void typeOfShoes()
+    {
+        System.out.println("These are professional shoes.");
+    } // End typeOfShoes.
+} // End ProfessionalShoes.
