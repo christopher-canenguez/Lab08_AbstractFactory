459ac6eb0759ec92f9f34f03c6611d473e9bb929
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-21 17:54:40 -0400

    Added party shoes class that implements Shoes.

---
+++ src/main/java/com/mycompany/lab08_abstractfactory/PartyShoes.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab08_abstractfactory;
+
+/**
+ *
+ * @author Chris Canenguez
+ */
+public class PartyShoes implements Shoes
+{
+    @Override
+    public void typeOfShoes()
+    {
+        System.out.println("These are party shoes.");
+    } // End typeOfShoes.
+} // End PartyShoes.
