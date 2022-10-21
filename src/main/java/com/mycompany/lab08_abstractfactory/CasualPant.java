e63a5c79ebcc122c9712cc15a74d9d4808baf365
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-21 17:48:44 -0400

    Added casual pant class that implements Pant.

---
+++ src/main/java/com/mycompany/lab08_abstractfactory/CasualPant.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab08_abstractfactory;
+
+/**
+ *
+ * @author Chris Canenguez
+ */
+public class CasualPant implements Pant
+{
+    @Override
+    public void typeOfPant()
+    {
+        System.out.println("This is a casual pant.");
+    } // End typeOfPant.
+} // End CasualPant.
