49f3ec57366a55097b633af5e1254508163cdfca
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-21 17:35:19 -0400

    Created Pant interface.

---
+++ src/main/java/com/mycompany/lab08_abstractfactory/Pant.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab08_abstractfactory;
+
+/**
+ *
+ * @author canen
+ */
+public interface Pant 
+{
+    // Every type of pant will display which family it belongs to.
+    void typeOfPant();
+} // End Pant interface.
