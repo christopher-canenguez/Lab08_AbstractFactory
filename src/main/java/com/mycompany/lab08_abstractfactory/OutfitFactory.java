7df46c01a51a38ec414e7e43f672923099beacd8
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-21 18:05:43 -0400

    Created Outfit Factory.

---
+++ src/main/java/com/mycompany/lab08_abstractfactory/OutfitFactory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab08_abstractfactory;
+
+/**
+ *
+ * @author Chris Canenguez
+ */
+public interface OutfitFactory 
+{
+    void showTop();
+    void showPant();
+    void showShoes();
+} // End OutfitFactory.
