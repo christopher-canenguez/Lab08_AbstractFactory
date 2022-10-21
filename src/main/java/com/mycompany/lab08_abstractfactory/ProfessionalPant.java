c7aed6f44369855e11f7386d4924856a848cbefe
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-21 17:47:51 -0400

    Added professional pant class that implements Pant.

---
+++ src/main/java/com/mycompany/lab08_abstractfactory/ProfessionalPant.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab08_abstractfactory;
+
+/**
+ *
+ * @author Chris Canenguez
+ */
+public class ProfessionalPant implements Pant
+{
+    @Override
+    public void typeOfPant()
+    {
+        System.out.println("This is a professional pant.");
+    } // End typeOfPant.
+} // End ProfessionalPant.
