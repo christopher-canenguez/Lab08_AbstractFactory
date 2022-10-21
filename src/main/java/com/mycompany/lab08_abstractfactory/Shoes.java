b750d0dd1137e22b210dcea87b4c0ef0c47f75f1
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-21 17:36:23 -0400

    Created Shoes interface.

---
+++ src/main/java/com/mycompany/lab08_abstractfactory/Shoes.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab08_abstractfactory;
+
+/**
+ *
+ * @author canen
+ */
+public interface Shoes 
+{
+    // Every type of shoe will display which family it belongs to.
+    void typeOfShoes();
+} // End Shoes interface.
