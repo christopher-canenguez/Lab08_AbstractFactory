d303db98de20dd80bedd18a795bad50e13093353
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-21 17:49:39 -0400

    Added party pant class that implements Pant.

---
+++ src/main/java/com/mycompany/lab08_abstractfactory/PartyPant.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab08_abstractfactory;
+
+/**
+ *
+ * @author Chris Canenguez
+ */
+public class PartyPant implements Pant
+{
+    @Override
+    public void typeOfPant()
+    {
+        System.out.println("This is a party pant.");
+    } // End typeOfPant.
+} // End PartyPant.
